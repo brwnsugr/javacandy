0576dbd45d15773b52d189b3df475a8814d9fe6a
GUESS <guess@GUESSui-iMac.local>
2020-07-10 00:26:34 +0900

    first commit

---
+++ src/main/java/com/devho/javatoy/JavatoyApplication.java
@@ -0,0 +1,10 @@
+package com.devho.javatoy;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatoyApplication {
+    public static void main(String[] args){
+        SpringApplication.run(JavatoyApplication.class, args);
+    }
+}
